609119c51fb15a2d2347b80a1ab169928f191b74
Tshepo <monenetshepo17@gmail.com>
2023-03-01 00:46:11 -0800

    pointer arrays and functions

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,8 +1,17 @@
+#include "main.h"
 
-                {
-                        _putchar('\n');
-                        break;
-                }
-                _putchar(str[count]);
-                count++;
-        }
+/**
+  * _puts - Prints a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
