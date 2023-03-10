8946b1092e26a0d491fd74601a6cf8276a27c791
Tshepo <monenetshepo17@gmail.com>
2023-03-10 01:33:53 -0800

    argc and argv

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,17 +1,15 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - A program that prints all arguements it receives
- * @argc: The arguement counter
- * @argv: The arguement values
- * Return: Always 0 (Success)
+ * main - prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	while (argc--)
-	{
-		printf("%s\n", *argv++);
-	}
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 	return (0);
 }
