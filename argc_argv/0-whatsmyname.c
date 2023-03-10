168536e606cc1bd663c28d95f9344041cafe7372
Tshepo <monenetshepo17@gmail.com>
2023-03-10 01:31:20 -0800

    argc and argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,12 @@
-#include "main.h"
 #include <stdio.h>
-
 /**
- * main - a program that prints its name, followed by a new line
- * @argc: arguement count
- * @argv: argument value, a strings that come after calling function
- * Return: Always 0 (Success)
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
-
+	printf("%s\n", argv[0]);
 	return (0);
 }
